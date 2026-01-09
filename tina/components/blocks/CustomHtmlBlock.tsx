d48f39878eade2135a46929792ac5b9309ca23bf
joerhoney <6787748+joerhoney@users.noreply.github.com>
2026-01-09 13:20:54 -0800

    Supressed Hydration Warning without failling to render HTML on the frontend.

--- tina/components/blocks/CustomHtmlBlock.tsx
+++ tina/components/blocks/CustomHtmlBlock.tsx
@@ -1,5 +1,4 @@
 import { tinaField } from "tinacms/dist/react";
-// import { useEffect, useRef } from "react";
 
 type CustomHtmlBlockProps = {
   block: any;
@@ -14,23 +13,14 @@ export default function CustomHtmlBlock({
 }: CustomHtmlBlockProps) {
   const blockClassName = block.className || "";
   const BlockTag = isGrouped ? "div" : "section";
-  // const containerRef = useRef<HTMLDivElement | HTMLElement>(null);
-
-  // useEffect(() => {
-  //   // Set HTML on client side only to avoid hydration mismatch
-  //   if (containerRef.current && block.html) {
-  //     containerRef.current.innerHTML = block.html;
-  //   }
-  // }, [block.html]);
 
   return (
     <BlockTag
       key={blockKey}
-      // ref={containerRef as any}
       className={`custom-html ${blockClassName}`}
       data-tina-field={tinaField(block, "html")}
       dangerouslySetInnerHTML={{ __html: block.html || "" }}
-      // suppressHydrationWarning
+      suppressHydrationWarning
     />
   );
 }
