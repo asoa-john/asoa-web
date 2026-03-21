799d048bc10727d74a49684fd5883a1a178c5d1f
joerhoney <6787748+joerhoney@users.noreply.github.com>
2026-03-21 10:01:51 -0700

    Migrated more blog posts. Fixed a bug. Minimal styling on Blog.

--- src/content.config.ts
+++ src/content.config.ts
@@ -30,7 +30,7 @@ const blog = defineCollection({
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().nullish(),
-    heroImageAlt: z.string().optional(),
+    heroImageAlt: z.string().nullish(),
     author: z.string().optional(),
     tags: z.array(z.string()).optional(),
     body: z.any(),
